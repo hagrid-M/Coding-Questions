06b4f78ca13cd3a1b7e13ff2b967bafbb18cce54
Megha Bajaj <bajajmeghaofficial@gmail.com>
2025-03-19 14:53:55 +0530

    Medium level array questions

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,19 +9,36 @@ public class Main {
             System.out.print(arr[i] + " ");
         System.out.println();
     }
+    static void printMatrix(int[][] matrix){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+    static void printList(List<Integer> result){
+        for(int elem: result){
+            System.out.print(elem + " ");
+        }
+
+    }
 
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
         int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for(int j=0;j<n;j++)
+                a[i][j] = sc.nextInt();
         }
         ArrayMedium ar = new ArrayMedium();
-        System.out.println(ar.maxProfit(a));
-
+        System.out.println(ar.spiralOrder(a));
 
     }
 }
