69f69e7bf58629e361cab09959976db9c897d6b7
Megha Bajaj <bajajmeghaofficial@gmail.com>
2025-05-11 11:29:50 +0530

    kth missing

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -330,4 +330,18 @@ public class BinarySearch {
         }
         return high;
     }
+    int findKthPositive(int[] arr, int k) {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int missing = arr[mid]-(mid+1);
+            if(missing<k)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return low+k;
+
+    }
 }
